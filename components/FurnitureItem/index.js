ced6c3d130ed290be2553ff8b6295d9c97252305
Harris Butler <hbutler5@patriots.uttyler.edu>
2021-03-25 14:35:09 -0500

    added buttons

--- components/FurnitureItem/index.js
+++ components/FurnitureItem/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {View, Text, ImageBackground} from 'react-native';
+import StyledButton from '../StyledButton'; 
 import styles from './styles.js';  
 
-const FurnitureItem = () => {
+const FurnitureItem = (props) => {
     return (
     <View style={styles.furnitureContainer}>
 
@@ -15,6 +16,22 @@ const FurnitureItem = () => {
           <Text style={styles.title}>Chiavari Chair</Text>
           <Text style={styles.subtitle}>Check availibility in your area.</Text>
         </View>
+
+        <StyledButton 
+          type="primary" 
+          content={"View in AR"}
+          onPress={() => {
+            console.warn('View AR was Pressed');
+          }}
+        />
+
+        <StyledButton 
+          type="secondary" 
+          content={"Learn More"}
+          onPress={() => {
+            console.warn('Learn More was Pressed');
+          }}
+        />
 
     </View>
     ); 
