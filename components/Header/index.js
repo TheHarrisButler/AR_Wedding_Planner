1cdf38217f88376fca7a5eb192d78fa0f5e70d5b
Harris Butler <hbutler5@patriots.uttyler.edu>
2021-04-08 17:55:02 -0500

    added log out

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,13 +1,20 @@
 import React from 'react'; 
-import {View, Image} from 'react-native';
-
+import {View, Image, Pressable} from 'react-native';
+import { Auth } from 'aws-amplify';
 import styles from './styles';
 
-const Header = () => {
+const Header = (props) => {
+
+    const logout = () => {
+        Auth.signOut();  
+    }
     return (
         <View style={styles.container}>
             <Image style={styles.logo} source={require('../../assets/silicon_wedding1.png')}/>
-            <Image style={styles.menu} source={require('../../assets/menu.png')}/>
+
+            <Pressable onPress={logout}>
+                <Image style={styles.menu} source={require('../../assets/menu.png')}/>
+            </Pressable>  
         </View>
     );
 };
