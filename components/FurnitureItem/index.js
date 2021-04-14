f7fe8e2d527ecc3dc0342f35094a76f8d1f879c9
Harris Butler <hbutler5@patriots.uttyler.edu>
2021-04-14 12:32:47 -0500

    added modal

--- components/FurnitureItem/index.js
+++ components/FurnitureItem/index.js
@@ -1,43 +1,69 @@
-import React from 'react';
-import {View, Text, ImageBackground} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, ImageBackground, Alert, Modal, Pressable} from 'react-native';
 import StyledButton from '../StyledButton'; 
 import styles from './styles.js';  
 
 
 const FurnitureItem = (props) => {
 
-    const { name, image } = props.furniture; 
+    const { name, image } = props.furniture;
+    
+    const [modalVisible, setModalVisible] = useState(false); 
 
     return (
     <View style={styles.furnitureContainer}>
 
-        <ImageBackground
-          source={image}
-          style={styles.image}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+
+          <View style={styles.modalView}>
+
+            <Text style={styles.modalText}>Hello World</Text>
+
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Back</Text>
+            </Pressable>
+
+          </View>
+        </View>
+      </Modal>
+
+      <ImageBackground
+        source={image}
+        style={styles.image}
+      />
+
+      <View style={styles.titles}>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.subtitle}>Check availibility in your area.</Text>
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <StyledButton 
+          type="primary" 
+          content={"View in AR"}
+          onPress={() => {
+            console.warn('View AR was Pressed');
+          }}
         />
 
-        <View style={styles.titles}>
-          <Text style={styles.title}>{name}</Text>
-          <Text style={styles.subtitle}>Check availibility in your area.</Text>
-        </View>
-
-        <View style={styles.buttonContainer}>
-          <StyledButton 
-            type="primary" 
-            content={"View in AR"}
-            onPress={() => {
-              console.warn('View AR was Pressed');
-            }}
-          />
-
-          <StyledButton 
-            type="secondary" 
-            content={"Learn More"}
-            onPress={() => {
-              console.warn('Learn More was Pressed');
-            }}
-          />
-        </View>
+        <StyledButton 
+          type="secondary" 
+          content={"Learn More"}
+          onPress={() => setModalVisible(true)}
+        />
+      </View>
 
     </View>
     ); 
